b62bff7f33480761a508352749b34a034e1d30a4
svdrski <svdrski@gmail.com>
2023-08-23 08:23:31 +0300

    2308

--- week7/day3/ex/script.js
+++ week7/day3/ex/script.js
@@ -1,17 +1,52 @@
-const arr = [
-  {id:1, name:'iPhone', price:850},
-  {id:2, name:'iPad', price:750},
-  {id:3, name:'iWatch', price:650}
-];
+// const arr = [
+//   {id:1, name:'iPhone', price:850},
+//   {id:2, name:'iPad', price:750},
+//   {id:3, name:'iWatch', price:650}
+// ];
 
-let newaa = arr.forEach((value) => {
-  console.log( `id is ${value.id} name is ${value.name} price is ${value.price}` )
-})
+// let newaa = arr.forEach((value) => {
+//   console.log( `id is ${value.id} name is ${value.name} price is ${value.price}` )
+// })
 
-const words = ["wow","hey","bye"];
+// const words = ["wow","hey","bye"];
 
 
-let checl = words.some((value) => value[0] === 'h' )
+// let checl = words.some((value) => value[0] === 'h' )
 
-console.log(checl)
+// console.log(checl)
 
+// (function print () {
+//   console.log("hello suka")
+// })()
+
+
+
+// print2()
+// function print2 () {
+//   console.log("AAaaa")
+// }
+
+// (function(greeting){
+//   console.log(greeting);
+// }('hey'))
+
+// (function fpintaa (value) {
+//   console.log(value)
+// }('sukkkka'))
+
+// ((a) => {
+//   console.log(a); // prints 10
+// })(10)
+
+
+function add() {
+  let counter = 0;  // create counter as number
+  /////----------------------
+  function plus() {   // create new function
+      counter += 1;    //  add 1 to counter
+      console.log(counter) ; //console log counter
+  }
+  plus();  // start function plus - (counter + 1)
+}
+
+add() // start function - result - console log 1
