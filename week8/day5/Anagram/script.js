c709c80965c0577125d802904d36dffb40a9a58f
svdrski <svdrski@gmail.com>
2023-08-31 20:14:43 +0300

    mini2

--- week8/day5/Anagram/script.js
+++ week8/day5/Anagram/script.js
@@ -2,16 +2,19 @@ let word1 = 'Astronomer'
 let word2 = 'Moon starer'
 
 function checker (word1, word2) {
-  let c = word1.toLowerCase()
-  let d = word2.toLowerCase()
-  if (c.length === d.length) {
-    let a = [...c]
-    let b = [...d]
-    let result = a.filter(value => b.includes(value))
-  }
-
-  
-  
+  let a = [...word1.toLowerCase().replace(/\s+/g, '')]
+  let b = [...word2.toLowerCase().replace(/\s+/g, '')]
+  let c = [...b]
+for (value of a) {
+  if (b.includes(value)) {
+    b.splice(b.indexOf(value),1)
+  } 
+}
+  a.length === c.length ? b.length === 0 ? console.log(true) : console.log(false) : console.log(false)
 }
 
 checker (word1, word2)
+checker ("School master", "The classroom")
+checker ("The Morse Code", "Here come dots")
+checker ("The Morsz Code", "Here come dots")
+
