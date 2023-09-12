f105e44e9e2d6082933d8cde9880d80a385a6c23
svdrski <svdrski@gmail.com>
2023-09-12 16:55:41 +0300

    xpgold

---
+++ week10/day2/XPGold/script.js
@@ -0,0 +1,15 @@
+const urls = [
+    'https://www.swapi.tech/api/people/1',
+    'https://www.swapi.tech/api/people/2',
+    'https://www.swapi.tech/api/people/3',
+    'https://www.swapi.tech/api/people/4'
+]
+
+let result = urls.map(value => fetch(value))
+
+Promise.all(result)
+    .then(a => Promise.all(a.map(c => c.json())))
+    .then(a => a.map(value => value.result.properties))
+    .then(a => console.log(a))
+    .catch(error => console.log('Error >', error))
+
